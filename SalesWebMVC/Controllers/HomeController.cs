4f3c10e607080c0451ed7b4510165d813472732b
CARLA REGINA MENDES <46799894+carlareginam10@users.noreply.github.com>
2023-02-08 21:15:32 -0300

    Firt controller and Razor pages Tests

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,20 +17,25 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
+            ViewData["Message"] = "Salles Web MVC App from C# Course.";
+            ViewData["email"] = "carlareginam10@gmail.com";
+            ViewData["name"] = "Carla Regina Mendes";
             return View();
         }
 
         public IActionResult Contact()
         {
+           
             ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
-        public IActionResult Privacy()
-        {
+        public IActionResult Privacy()            {
+
+            //chave e valor - o sistema vai procurar uma view c=hamada About e colocar essa msg dentro da tag ["Message"]
+            ViewData["Message"] = "2 - estando o uso de chave e valor - essa msg foi escrita dentro do HomeController, mas a montagem dela em tela se deu por meio da view about";
+            ViewData["xuxu"] = "3 - coloquei XUXU no lugar de Message pra testar";
+            ViewData["NOTES"] = "1 - QUANDO COLOCA @{} NA VIEW QUER DIZER QUE POSSO COLOCAR QUALQUER CODIDO C# DENTRO DAS CHAVES";
             return View();
         }
 
